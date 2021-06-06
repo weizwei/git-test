e9fc6b701f2d3df54638ca4e06840dc758d6e754
wzw <1105798051@qq.com>
2021-06-06 14:17:39 +0800

    git test

---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git1");
+        System.out.println("git2");
+        System.out.println("git3");
+    }
+}
